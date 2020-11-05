43422e3f738f465e6bb24bddbd2312d385e74327
kaddy120 <kaddy120@gmail.com>
2020-11-05 15:39:41 +0200

    Initial commit

---
+++ Ecommerce/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
